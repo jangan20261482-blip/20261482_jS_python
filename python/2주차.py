e0694a3ed5806551c9983fc1343d2ef09dbb9100
haseong <jangan20261482@GMAIL.COM>
2026-09-08 16:03:01 +0900

    하위 폴더 및 파일 추가

---
+++ python/2주차.py
@@ -0,0 +1,19 @@
+print("1 - Hello,world!")
+print(30 * 30)
+
+
+a = str(input("첫번째 값 :"))
+b = str(input("두번째 값 :"))
+
+
+
+
+## result = a*b
+## result1 = a/b
+## result2 = a+b
+## result3 = a-b
+
+##print(result)
+print(a, "and", b )
+
+    
